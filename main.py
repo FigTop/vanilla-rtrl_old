4dd7ac99a31588448ef9ddb0a989b5ee684330e1
omarschall <owen.marschall@gmail.com>
2018-10-18 18:17:56 -0400

    test

--- main.py
+++ main.py
@@ -17,17 +17,17 @@ from analysis_funcs import *
 from learning_algorithms import *
 
 i_seed = np.random.randint(100)
-np.random.seed(33)
+np.random.seed(72)
 
 task = Coin_Task(3, 5, one_hot=True, deterministic=False)
-data = task.gen_data(15000, 1000)
+data = task.gen_data(15000, 10000)
 #task = Copy_Task(10, 3)
 
 n_in     = task.n_in
 n_hidden = 32
 n_out    = task.n_out
 
-W_in  = np.random.normal(0, np.sqrt(1/(n_in)), (n_hidden, n_in))
+W_in  = np.random.normal(0, np.sqrt(1/(2*n_in)), (n_hidden, n_in))
 W_rec = np.random.normal(0, np.sqrt(1/(2*n_hidden)), (n_hidden, n_hidden))
 W_out = np.random.normal(0, np.sqrt(1/(n_hidden)), (n_out, n_hidden))
 
@@ -44,10 +44,11 @@ rnn = RNN(W_in, W_rec, W_out, b_rec, b_out,
 
 optimizer = SGD(lr=0.001, clipnorm=1.0)
 SG_optimizer = SGD(lr=0.01)
-learn_alg = DNI(rnn, SG_optimizer, activation=identity,
+learn_alg = DNI(rnn, SG_optimizer, activation=tanh,
                 monitors=['sg_loss', 'sg', 'A', 'B', 'C', 'SG_grads'],
                 lambda_mix=0, l2_reg=0, fix_SG_interval=5)
-monitors = ['loss_', 'a', 'W_rec']
+comp_alg = BPTT(rnn, 1, 10)
+monitors = ['loss_', 'a', 'W_rec', 'alignment']
 
 rnn.run(data,
         learn_alg=learn_alg,
@@ -55,15 +56,16 @@ rnn.run(data,
         monitors=monitors,
         update_interval=1,
         l2_reg=0.01,
-        check_accuracy=False,
+        check_accuracy=True,
+        comparison_alg=comp_alg,
         verbose=True)
 
 fig = plt.figure()
-signals = [rnn.mons['loss_'], learn_alg.mons['sg_loss']]
+signals = [rnn.mons['loss_'], learn_alg.mons['sg_loss'], rnn.mons['alignment']]
 #for signal in signals:
 #    plt.plot(signal)
-task.plot_filtered_signals(signals, y_lim=[0, 1.5])
-plt.legend(['Loss', 'SG Loss'])
+task.plot_filtered_signals(signals, y_lim=[0, 1.5], filter_size=200)
+plt.legend(['Loss', 'SG Loss', 'alignment'])
 
 if False:
     fig2 = plt.figure()
@@ -71,6 +73,8 @@ if False:
     for obj in [rnn, learn_alg]:
         mons = getattr(obj, 'mons')
         for key in mons.keys():
+            if key=='loss_':
+                continue
             signal = []
             if 'grads' not in key:
                 for i in range(len(mons[key])):
@@ -90,8 +94,8 @@ if False:
     plt.plot(W_radii)
     leg.append('W_radii')
             
-    plt.xlim([0, 115])
-    plt.ylim([0, 20])
+    plt.xlim([2000, 2259])
+    plt.ylim([0, 2])
     plt.legend(leg)
 
 #W = np.nan_to_num(np.array(rnn.mons['W_rec']))
