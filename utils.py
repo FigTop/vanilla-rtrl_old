0c4b37aa7ddf5fbf1044f9a3831e51458242b93f
omarschall <owen.marschall@gmail.com>
2019-01-03 12:09:55 -0500

    New utils.py module

---
+++ utils.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  3 12:08:30 2019
+
+@author: omarschall
+"""
+
+import numpy as np
+
+def norm(z):
+    
+   return np.sqrt(np.sum(np.square(z))) 
