3d13f6941a39497ef140a5f5060bd3e179d3d4e3
omarschall <owen.marschall@gmail.com>
2020-01-28 15:54:15 -0500

    Add process job function

--- submit_jobs.py
+++ submit_jobs.py
@@ -8,6 +8,7 @@ Created on Thu Oct 18 19:19:46 2018
 
 import subprocess
 import os
+import numpy as np
 
 def clear_results(job_file, data_path='/Users/omarschall/cluster_results/vanilla-rtrl/'):
     
@@ -116,3 +117,57 @@ def write_job_file(job_name,
             )
             
     return job_file
+
+def process_results(job_file):
+    
+    job_name = job_file.split('/')[-1].split('.')[0]
+    data_path = os.path.join('/Users/omarschall/cluster_results/vanilla-rtrl/',
+                             job_name)
+    dir_list = os.listdir(data_path)
+    dir_list.pop(dir_list.index('code'))
+    
+    max_seed = 0
+    
+    for i_file, file in enumerate(dir_list):
+        
+        with open(os.path.join(data_path, file), 'rb') as f:
+            data = pickle.load(f)
+            
+        if i_file == 0:
+            
+            #configs_array = [[]] * len(data['config'].keys() + 1)
+            configs_array = {key : [] for key in data['config'].keys()}
+            key_order = [key for key in data['config'].keys()]
+            
+        for key in data['config'].keys():
+            if data['config'][key] not in configs_array[key]:
+                configs_array[key].append(data['config'][key])
+        
+        max_seed = np.maximum(max_seed, data['i_seed'])
+    
+    configs_array['i_seed'] = list(range(max_seed + 1))
+    key_order.append('i_seed')
+        
+    for key in configs_array.keys():
+        
+        configs_array[key] = sorted(configs_array[key])
+        
+    results_array = np.zeros([len(configs_array[key]) for key in key_order])
+    
+    #Put data in array
+    for i_file, file in enumerate(dir_list):
+        
+        with open(os.path.join(data_path, file), 'rb') as f:
+            data = pickle.load(f)
+            
+        index = []
+        for key in key_order:
+            try:
+                index.append(configs_array[key].index(data['config'][key]))
+            except KeyError:
+                index.append(data['i_seed'])
+        index = tuple(index)
+        #set_trace()
+        results_array[index] = data['processed_data']['test_loss']
+        
+    return configs_array, results_array, key_order
