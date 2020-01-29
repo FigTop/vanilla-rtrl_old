6c4a464fc2f770787d0f2f53c5edfd2eb444ad9c
omarschall <owen.marschall@gmail.com>
2020-01-29 13:33:26 -0500

    Clean up slightly

--- submit_jobs.py
+++ submit_jobs.py
@@ -11,52 +11,52 @@ import os
 import numpy as np
 
 def clear_results(job_file, data_path='/Users/omarschall/cluster_results/vanilla-rtrl/'):
-    
+
     job_name = job_file.split('/')[-1].split('.')[0]
     data_dir = os.path.join(data_path, job_name)
-    
+
     subprocess.run(['rm', data_dir+'/*_*'])
 
 def retrieve_results(job_file, scratch_path='/scratch/oem214/vanilla-rtrl/',
                username='oem214', domain='prince.hpc.nyu.edu'):
-    
+
     job_name = job_file.split('/')[-1].split('.')[0]
     data_path = '/Users/omarschall/cluster_results/vanilla-rtrl/'
     data_dir = os.path.join(data_path, job_name)
-        
+
     source_path = username+'@'+domain+':'+scratch_path+'library/'+job_name+'/'
-        
+
     subprocess.run(['rsync', '-aav', source_path, data_dir])
 
 def submit_job(job_file, n_array, scratch_path='/scratch/oem214/vanilla-rtrl/',
                username='oem214', domain='prince.hpc.nyu.edu'):
-    
+
     job_name = job_file.split('/')[-1]#
     data_path = '/Users/omarschall/cluster_results/vanilla-rtrl/'
     data_dir = os.path.join(data_path, job_name.split('.')[0])
-    
+
     if not os.path.exists(data_dir):
         os.mkdir(data_dir)
         code_dir = os.path.join(data_dir, 'code')
         os.mkdir(code_dir)
-    
+
     code_dir = os.path.join(data_dir, 'code')
-    
+
     #copy main script to results dir
     subprocess.run(['rsync',
                     '-aav',
                     '--exclude', '.git',
                     '/Users/omarschall/vanilla-rtrl/',
                     code_dir])
-    
+
     subprocess.run(['rsync',
                     '-aav',
                     '--exclude', '.git',
                     '/Users/omarschall/vanilla-rtrl/',
                     username+'@'+domain+':'+scratch_path])
-    
+
     subprocess.run(['scp', job_file, username+'@'+domain+':/home/oem214/'])
-    
+
     subprocess.run(['ssh', username+'@'+domain,
                     'sbatch', '--array=1-'+str(n_array), job_name])
 
@@ -115,51 +115,50 @@ def write_job_file(job_name,
             + 'which python >> {}.log\n'.format(log_name)
             + 'python main.py\n'
             )
-            
+
     return job_file
 
 def process_results(job_file):
-    
+
     job_name = job_file.split('/')[-1].split('.')[0]
     data_path = os.path.join('/Users/omarschall/cluster_results/vanilla-rtrl/',
                              job_name)
     dir_list = os.listdir(data_path)
     dir_list.pop(dir_list.index('code'))
-    
+
     max_seed = 0
-    
+
     for i_file, file in enumerate(dir_list):
-        
+
         with open(os.path.join(data_path, file), 'rb') as f:
             data = pickle.load(f)
-            
+
         if i_file == 0:
-            
-            #configs_array = [[]] * len(data['config'].keys() + 1)
+
             configs_array = {key : [] for key in data['config'].keys()}
             key_order = [key for key in data['config'].keys()]
-            
+
         for key in data['config'].keys():
             if data['config'][key] not in configs_array[key]:
                 configs_array[key].append(data['config'][key])
-        
+
         max_seed = np.maximum(max_seed, data['i_seed'])
-    
+
     configs_array['i_seed'] = list(range(max_seed + 1))
     key_order.append('i_seed')
-        
+
     for key in configs_array.keys():
-        
+
         configs_array[key] = sorted(configs_array[key])
-        
+
     results_array = np.zeros([len(configs_array[key]) for key in key_order])
-    
+
     #Put data in array
     for i_file, file in enumerate(dir_list):
-        
+
         with open(os.path.join(data_path, file), 'rb') as f:
             data = pickle.load(f)
-            
+
         index = []
         for key in key_order:
             try:
@@ -169,5 +168,5 @@ def process_results(job_file):
         index = tuple(index)
         #set_trace()
         results_array[index] = data['processed_data']['test_loss']
-        
+
     return configs_array, results_array, key_order
